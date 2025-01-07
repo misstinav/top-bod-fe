b88dd4044d7a9d107800ff305f250dc850fb584c
Valentina <dysfnalv@gmail.com>
2025-01-06 21:46:16 -0600

    added the search service

---
+++ src/app/services/search-service/search-service.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-service',
+  imports: [],
+  templateUrl: './search-service.component.html',
+  styleUrl: './search-service.component.css'
+})
+export class SearchServiceComponent {
+
+}
