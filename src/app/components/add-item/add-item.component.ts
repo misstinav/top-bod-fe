5760d67540f92764fcac4780749dbbcfc973c16b
Valentina <dysfnalv@gmail.com>
2025-01-06 21:33:54 -0600

    created the initial search, add, and meal components

---
+++ src/app/components/add-item/add-item.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-item',
+  imports: [],
+  templateUrl: './add-item.component.html',
+  styleUrl: './add-item.component.css'
+})
+export class AddItemComponent {
+
+}
---
+++ src/app/components/meal/meal.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-meal',
+  imports: [],
+  templateUrl: './meal.component.html',
+  styleUrl: './meal.component.css'
+})
+export class MealComponent {
+
+}
