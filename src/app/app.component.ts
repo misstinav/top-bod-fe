7003b706ba943dddb0c94f4462761a1bbb0c94e3
Valentina <dysfnalv@gmail.com>
2025-01-08 21:39:11 -0600

    removed unused import and added debugging styling to app css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
 import { FooterComponent } from "./footer/footer.component";
 import { DayComponent } from "./components/day/day.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, DayComponent],
+  imports: [HeaderComponent, FooterComponent, DayComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
