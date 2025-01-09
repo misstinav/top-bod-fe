bd2105b960a549ab81cc39ca29ed663a83398eaa
Valentina <dysfnalv@gmail.com>
2025-01-08 20:57:54 -0600

    Added header, footer, and day base. They are visible in the UI

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { MealComponent } from './components/meal/meal.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: MealComponent}
+];
--- src/app/services/search-service/search-service.component.ts
+++ src/app/services/search-service/search-service.component.ts
@@ -8,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class SearchServiceComponent {
 
+  async ngOnInit() {
+    const res = await fetch('backend link'); //added var that connects to backend endpoint
+    const data = await res.json();
+    //this.whateverVar.set(data) TODO: need to update the whatever var
+  }
 }
