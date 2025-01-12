00e41f9adaa8d8c0da2648d1230c13091988e557
Valentina <dysfnalv@gmail.com>
2025-01-11 22:33:29 -0600

    created data interface

---
+++ src/app/Interfaces/nutrition-details.ts
@@ -0,0 +1,9 @@
+export interface NutritionDetails {
+    id: number,
+    name: string,
+    calories: number,
+    servingSize: number,
+    total_fat: number,
+    protein: number,
+    carbohydrates: number,
+}
