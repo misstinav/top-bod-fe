90699058cdd922a1fdc359885b616eef7e984c91
Valentina <dysfnalv@gmail.com>
2025-01-30 21:43:11 -0600

    WIP: adding html and css to day component

--- src/app/components/day/day.component.ts
+++ src/app/components/day/day.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { MealComponent } from "../meal/meal.component";
 
 @Component({
   selector: 'app-day',
-  imports: [],
+  imports: [MealComponent],
   templateUrl: './day.component.html',
   styleUrl: './day.component.css'
 })
